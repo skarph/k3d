f6f47129d6a342a162b1d65289b6ccd5e4c17b0b
Skarph <theSkarph@gmail.com>
2023-11-18 00:17:23 -0500

    Added intentional crash to ActorMode, please dont use yet

--- scripts/objects/ActorModel.lua
+++ scripts/objects/ActorModel.lua
@@ -1,4 +1,7 @@
 local ActorModel = Class("Model")
 printt("Loading ActorModel")
     --TODO: everything
+function ActorModel.init(self)
+    error("ActorModel is not implemented yet :( sorry")
+end
 return ActorModel
